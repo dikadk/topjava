97345152812d56ed08bbd93c077a557bc8a52ee0
dkiro <dimakiro2105@gmail.com>
2017-04-04 13:50:42 -0400

    HW2 1

---
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserRepositoryImpl.java
@@ -0,0 +1,58 @@
+package ru.javawebinar.topjava.repository.mock;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.NamedEntity;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.UserRepository;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+@Repository
+public class InMemoryUserRepositoryImpl implements UserRepository {
+    private static final Logger LOG = LoggerFactory.getLogger(InMemoryUserRepositoryImpl.class);
+    private Map<Integer, User> repository = new ConcurrentHashMap<>();
+    private AtomicInteger counter = new AtomicInteger(0);
+
+    @Override
+    public boolean delete(int id) {
+        LOG.info("delete " + id);
+        repository.remove(id);
+        return true;
+    }
+
+    @Override
+    public User save(User user) {
+        LOG.info("save " + user);
+        if (user.isNew()) {
+            user.setId(counter.incrementAndGet());
+        }
+        repository.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public User get(int id) {
+        LOG.info("get " + id);
+        return repository.get(id);
+    }
+
+    @Override
+    public List<User> getAll() {
+        LOG.info("getAll");
+        return repository.values().stream().sorted(Comparator.comparing(User::getName).thenComparing(User::getEmail)).collect(Collectors.toList());
+    }
+
+    @Override
+    public User getByEmail(String email) {
+        LOG.info("getByEmail " + email);
+        return repository.values().stream().filter(user->user.getEmail().equalsIgnoreCase(email)).findAny().get();
+    }
+}
